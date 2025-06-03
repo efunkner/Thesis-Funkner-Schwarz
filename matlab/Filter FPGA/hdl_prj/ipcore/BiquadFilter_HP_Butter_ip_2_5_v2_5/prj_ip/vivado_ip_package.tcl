1771f1750a4f7bf2339a2912792f093153c7dde0
Zer0-3nergy <166703347+Zer0-3nergy@users.noreply.github.com>
2025-06-03 16:34:22 +0200

    some changes

--- matlab/Filter FPGA/hdl_prj/ipcore/BiquadFilter_HP_Butter_ip_2_5_v2_5/prj_ip/vivado_ip_package.tcl
+++ matlab/Filter FPGA/hdl_prj/ipcore/BiquadFilter_HP_Butter_ip_2_5_v2_5/prj_ip/vivado_ip_package.tcl
@@ -39,7 +39,7 @@ foreach family $Families {append IPSupportedFamily "{$family} {Production} "}
 set_property supported_families $IPSupportedFamily [ipx::current_core]
 set_property taxonomy {{/HDL Coder Generated IP}} [ipx::current_core]
 set_property description {HDL Coder generated IP} [ipx::current_core]
-set_property core_revision 2114110519 [ipx::current_core]
+set_property core_revision 2114111733 [ipx::current_core]
 
 # Add HDL source files to IP
 ipx::add_file {hdl/BiquadFilter_HP_Butter_ip_2_5_src_BiquadDF2Section1.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
