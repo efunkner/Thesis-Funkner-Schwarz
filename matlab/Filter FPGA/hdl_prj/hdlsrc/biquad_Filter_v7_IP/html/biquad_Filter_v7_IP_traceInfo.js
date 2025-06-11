56e40f2c937744194a6c09952399f8a96633f516
Zer0-3nergy <166703347+Zer0-3nergy@users.noreply.github.com>
2025-06-11 19:26:14 +0200

    new Design with 2 Filters + Updated Dev-Notes
    new Design with two new Filter in one Design

---
+++ matlab/Filter FPGA/hdl_prj/hdlsrc/biquad_Filter_v7_IP/html/biquad_Filter_v7_IP_traceInfo.js
@@ -0,0 +1,30 @@
+function RTW_Sid2UrlHash() {
+	this.urlHashMap = new Array();
+	/* <S1>/Biquad Filter */
+	this.urlHashMap["biquad_Filter_v7_IP:71"] = "Butter_HP_4_2500_v1_src_Biquad_Filtrer.vhd:95,96,97,98,99,100,101,102,103";
+	/* <S1>/Biquad Filter1 */
+	this.urlHashMap["biquad_Filter_v7_IP:83"] = "Butter_HP_4_2500_v1_src_Biquad_Filtrer.vhd:105,106,107,108,109,110,111,112,113";
+	this.getUrlHash = function(sid) { return this.urlHashMap[sid];}
+}
+RTW_Sid2UrlHash.instance = new RTW_Sid2UrlHash();
+function RTW_rtwnameSIDMap() {
+	this.rtwnameHashMap = new Array();
+	this.sidHashMap = new Array();
+	this.rtwnameHashMap["<Root>"] = {sid: "biquad_Filter_v7_IP"};
+	this.sidHashMap["biquad_Filter_v7_IP"] = {rtwname: "<Root>"};
+	this.rtwnameHashMap["<S1>/In1"] = {sid: "biquad_Filter_v7_IP:18"};
+	this.sidHashMap["biquad_Filter_v7_IP:18"] = {rtwname: "<S1>/In1"};
+	this.rtwnameHashMap["<S1>/In2"] = {sid: "biquad_Filter_v7_IP:72"};
+	this.sidHashMap["biquad_Filter_v7_IP:72"] = {rtwname: "<S1>/In2"};
+	this.rtwnameHashMap["<S1>/Biquad Filter"] = {sid: "biquad_Filter_v7_IP:71"};
+	this.sidHashMap["biquad_Filter_v7_IP:71"] = {rtwname: "<S1>/Biquad Filter"};
+	this.rtwnameHashMap["<S1>/Biquad Filter1"] = {sid: "biquad_Filter_v7_IP:83"};
+	this.sidHashMap["biquad_Filter_v7_IP:83"] = {rtwname: "<S1>/Biquad Filter1"};
+	this.rtwnameHashMap["<S1>/Out1"] = {sid: "biquad_Filter_v7_IP:19"};
+	this.sidHashMap["biquad_Filter_v7_IP:19"] = {rtwname: "<S1>/Out1"};
+	this.rtwnameHashMap["<S1>/Out2"] = {sid: "biquad_Filter_v7_IP:73"};
+	this.sidHashMap["biquad_Filter_v7_IP:73"] = {rtwname: "<S1>/Out2"};
+	this.getSID = function(rtwname) { return this.rtwnameHashMap[rtwname];}
+	this.getRtwname = function(sid) { return this.sidHashMap[sid];}
+}
+RTW_rtwnameSIDMap.instance = new RTW_rtwnameSIDMap();
---
+++ matlab/Filter FPGA/hdl_prj/ipcore/Butter_TP_4_1000_v1_v1_0/include/Butter_TP_4_1000_v1_addr.h
@@ -0,0 +1,15 @@
+/*
+ * File Name:         hdl_prj\ipcore\Butter_TP_4_1000_v1_v1_0\include\Butter_TP_4_1000_v1_addr.h
+ * Description:       C Header File
+ * Created:           2025-06-11 14:56:30
+*/
+
+#ifndef BUTTER_TP_4_1000_V1_H_
+#define BUTTER_TP_4_1000_V1_H_
+
+#define  IPCore_Reset_Butter_TP_4_1000_v1                           0x0  //write 0x1 to bit 0 to reset IP core
+#define  IPCore_Enable_Butter_TP_4_1000_v1                          0x4  //enabled (by default) when bit 0 is 0x1
+#define  IPCore_PacketSize_AXI4_Stream_Master_Butter_TP_4_1000_v1   0x8  //Packet size for AXI4-Stream Master interface, the default value is 262144. The TLAST output signal of the AXI4-Stream Master interface is generated based on the packet size.
+#define  IPCore_Timestamp_Butter_TP_4_1000_v1                       0xC  //contains unique IP timestamp (yymmddHHMM): 2506111456
+
+#endif /* BUTTER_TP_4_1000_V1_H_ */
