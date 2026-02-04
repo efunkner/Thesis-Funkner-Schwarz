4651152e000f03996fdf8bf7c61c94c67dd4af5d
Mirco Meiners <Mirco.Meiners@hs-bremen.de>
2026-02-04 20:12:47 +0100

    Clean up

--- filter_demonstration/mikrocontroller_demo/biquad_demo/README.md
+++ filter_demonstration/mikrocontroller_demo/biquad_demo/README.md
@@ -1,4 +1,4 @@
-# Filterdemonstration
+0# Filterdemonstration
 
 Das `biquad_demo.ino` Programm filtert ein generiertes Rechtecksignal mit einem Biquad-Filter. Die Arduino IDE ermöglicht im Serialplotter die gefilterte Impulsantwort zu betrachten.
 Für diese Demonstration werden keine weiteren Bibliotheken für die Arduino IDE benötigt und kann auf jedem Arduino fähigen Mikrocontroller verwendet werden.
--- filter_demonstration/mikrocontroller_demo/biquad_demo/biquad_demo.ino
+++ filter_demonstration/mikrocontroller_demo/biquad_demo/biquad_demo.ino
@@ -1,35 +1,35 @@
 // --- Hilfsklasse für Filter
 class Filter
-  {
-  public:
-    virtual ~Filter() {}
-    virtual float filter(float) = 0;
-  };
+{
+ public:
+	virtual ~Filter() {}
+	virtual float filter(float) = 0;
+};
 
 // --- Biquad-Filter in Direct Form 1
 class BiquadFilterDF1 : public Filter
-  {
-  private:
-    const float b_0; 
-    const float b_1;
-    const float b_2;
-    const float a_1;
-    const float a_2;
-
-    float x_1 = 0;
-    float y_1 = 0;
-    float y_2 = 0;
-
-  public:
-    BiquadFilterDF1(const float (&b)[3], const float (&a)[3], float gain)  
-        : b_0(gain * b[0] / a[0]),
-          b_1(gain * b[1] / a[0]),
-          b_2(gain * b[2] / a[0]),
-          a_1(a[1] / a[0]),
-          a_2(a[2] / a[0]) 
+{
+ private:
+	const float b_0; 
+	const float b_1;
+	const float b_2;
+	const float a_1;
+	const float a_2;
+	
+	float x_1 = 0;
+	float y_1 = 0;
+	float y_2 = 0;
+	
+ public:
+ BiquadFilterDF1(const float (&b)[3], const float (&a)[3], float gain)  
+	 : b_0(gain * b[0] / a[0]),
+		b_1(gain * b[1] / a[0]),
+		b_2(gain * b[2] / a[0]),
+		a_1(a[1] / a[0]),
+		a_2(a[2] / a[0]) 
     {
     }
-
+	
   float filter(float x_0)
   {
     float x_2 = x_1;
@@ -39,76 +39,76 @@ class BiquadFilterDF1 : public Filter
     
     y_2 = y_1;
     y_1 = y_0;
-
+		
     return y_0;
   }  
 };
 
 // --- Biquad-Filter in Direct Form 2 ---
 class BiquadFilterDF2 : public Filter
-  {
-  private:
-    // Filterkoeffizienten
-    const float b_0; 
-    const float b_1;
-    const float b_2;
-    const float a_1;
-    const float a_2;
-
-    float w_0 = 0;
-    float w_1 = 0;
-
-  public:
-    // Konstruktor mit Koeffizienten und Verstärkung (Gain)
-    BiquadFilterDF2(const float (&b)[3], const float (&a)[3], float gain)  
-        : b_0(gain * b[0] / a[0]),
-          b_1(gain * b[1] / a[0]),
-          b_2(gain * b[2] / a[0]),
-          a_1(a[1] / a[0]),
-          a_2(a[2] / a[0]) 
+{
+ private:
+	// Filterkoeffizienten
+	const float b_0; 
+	const float b_1;
+	const float b_2;
+	const float a_1;
+	const float a_2;
+	
+	float w_0 = 0;
+	float w_1 = 0;
+	
+ public:
+	// Konstruktor mit Koeffizienten und Verstärkung (Gain)
+ BiquadFilterDF2(const float (&b)[3], const float (&a)[3], float gain)  
+	 : b_0(gain * b[0] / a[0]),
+		b_1(gain * b[1] / a[0]),
+		b_2(gain * b[2] / a[0]),
+		a_1(a[1] / a[0]),
+		a_2(a[2] / a[0]) 
     {
     }
-
+	
   float filter(float x_0)
   {
     float w_2 = w_1;
     w_1 = w_0;
     w_0 = x_0 - a_1 * w_1 - a_2 * w_2;
-
+		
     //Differenzengleichung der Direct form 2
     float y_0 = b_0 * w_0 + b_1 * w_1 + b_2 * w_2;
-
+		
     return y_0;
   }  
 };
 
 // --- Transponierter Biquad-Filter in Direct Form 2 ---
 class BiquadFilterTDF2 : public Filter
-  {
-  private:
-    const float b_0; 
-    const float b_1;
-    const float b_2;
-    const float a_1;
-    const float a_2;
-
-    float s_1 = 0;
-    float s_2 = 0;
-
-  public:
-    BiquadFilterTDF2(const float (&b)[3], const float (&a)[3], float gain)  
-        : b_0(gain * b[0] / a[0]),
-          b_1(gain * b[1] / a[0]),
-          b_2(gain * b[2] / a[0]),
-          a_1(a[1] / a[0]),
-          a_2(a[2] / a[0]) 
+{
+ private:
+	const float b_0; 
+	const float b_1;
+	const float b_2;
+	const float a_1;
+	const float a_2;
+	
+	float s_1 = 0;
+	float s_2 = 0;
+	
+ public:
+ BiquadFilterTDF2(const float (&b)[3], const float (&a)[3], float gain)  
+	 : b_0(gain * b[0] / a[0]),
+		b_1(gain * b[1] / a[0]),
+		b_2(gain * b[2] / a[0]),
+		a_1(a[1] / a[0]),
+		a_2(a[2] / a[0]) 
     {
     }
-
+	
   float filter(float x_0)
   {
     float y_0 = s_1 + b_0 * x_0;
-
+		
     s_1 = s_2 + b_1 * x_0 - a_1 * y_0; 
     s_2 = b_2 * x_0 - a_2 * y_0;
     
@@ -117,7 +117,7 @@ class BiquadFilterTDF2 : public Filter
 };
 
 
-//Filterkoeffizienten für BS
+// Filterkoeffizienten für BS
 const float b_0 = 0.9560f;
 const float b_1 = -1.7243;
 const float b_2 = 0.9560f;
@@ -126,7 +126,7 @@ const float a_1 = -1.7243f;
 const float a_2 = 0.9121f;
 
 /*
-//Filterkoeffizienten für BP
+// Filterkoeffizienten für BP
 const float b_0 = -0.2066f;
 const float b_1 = 0.0000f;
 const float b_2 = 0.2066f;
@@ -136,7 +136,7 @@ const float a_2 = 0.9121f;
 */
 
 /*
-//Filterkoeffizienten für BP
+// Filterkoeffizienten für BP
 const float b_0 = 0.9091f;
 const float b_1 = -1.8182f;
 const float b_2 = 0.9091f;
@@ -151,7 +151,7 @@ const float gain = 1;
 
 BiquadFilterTDF2 biquad(b_coefficients, a_coefficients, gain);
 
-//==================================================================================================================================================================================
+//===========================================================
 // Parameter für das Rechtecksignal
 const float amplitudeHigh = 0.2;
 const float amplitudeLow = -0.2;
@@ -160,22 +160,22 @@ const unsigned long halfPeriod = period / 2;
 
 unsigned long lastToggleTime = 0;
 float signalValue = amplitudeHigh;
-//==================================================================================================================================================================================
+//===========================================================
 
 void setup() {
   Serial.begin(1200);
 }
 
 void loop() {
-  //RECHTECK SIGNAL
+  // RECHTECK SIGNAL
   unsigned long currentTime = millis();
   if (currentTime - lastToggleTime >= halfPeriod) {
     lastToggleTime = currentTime;
     signalValue = (signalValue == amplitudeHigh) ? amplitudeLow : amplitudeHigh;
   }
-  //Filterung des Rechtecksignals
+  // Filterung des Rechtecksignals
   float filtered = biquad.filter(signalValue); 
-
+	
   Serial.print(signalValue);
   Serial.print("\t");
   Serial.println(filtered);
